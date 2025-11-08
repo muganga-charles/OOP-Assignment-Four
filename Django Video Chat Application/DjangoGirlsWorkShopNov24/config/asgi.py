bd88e875f2cbb300684e25aba9eeb38c71829c1c
muganga-charles <mugangacharles5@gmail.com>
2025-11-08 20:57:22 +0300

    Adding django application

---
+++ Django Video Chat Application/DjangoGirlsWorkShopNov24/config/asgi.py
@@ -0,0 +1,30 @@
+"""
+ASGI config for config project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from chat.routing import websocket_urlpatterns
+from call.routing import call_websocket_urlpatterns # add this
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+django_asgi_app = get_asgi_application()
+
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AuthMiddlewareStack(
+            URLRouter(websocket_urlpatterns + call_websocket_urlpatterns) # add this
+        ),
+    }
+)
